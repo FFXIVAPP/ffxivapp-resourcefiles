af01730c56fb0f09893ab76a3611ee12aba5be39
Ryan Wilson <syndicated.life@gmail.com>
2018-07-04 08:53:03 -0700

    updates to 462 and license (#1)

--- FFXIVAPP.ResourceFiles/Game.cs
+++ FFXIVAPP.ResourceFiles/Game.cs
@@ -1,114 +1,172 @@
-﻿// FFXIVAPP.ResourceFiles ~ Game.cs
-// 
-// Copyright © 2007 - 2017 Ryan Wilson - All Rights Reserved
-// 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Game.cs" company="SyndicatedLife">
+//   Copyright(c) 2018 Ryan Wilson &amp;lt;syndicated.life@gmail.com&amp;gt; (http://syndicated.life/)
+//   Licensed under the MIT license. See LICENSE.md in the solution root for full license information.
+// </copyright>
+// <summary>
+//   Game.cs Implementation
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
-using System;
-using System.Windows.Media.Imaging;
+namespace FFXIVAPP.ResourceFiles {
+    using System;
+    using System.Windows.Media.Imaging;
 
-namespace FFXIVAPP.ResourceFiles
-{
-    public static class Game
-    {
+    public static class Game {
         public static BitmapImage Aetheryte = new BitmapImage(new Uri($"{Constants.IconsPack}/aetheryte.png"));
+
         public static BitmapImage Alchemist = new BitmapImage(new Uri($"{Constants.IconsPack}/alc.png"));
+
         public static BitmapImage Arcanist = new BitmapImage(new Uri($"{Constants.IconsPack}/acn.png"));
+
         public static BitmapImage Archer = new BitmapImage(new Uri($"{Constants.IconsPack}/arc.png"));
+
         public static BitmapImage Armorer = new BitmapImage(new Uri($"{Constants.IconsPack}/arm.png"));
+
         public static BitmapImage Astrologian = new BitmapImage(new Uri($"{Constants.IconsPack}/ast.png"));
+
         public static BitmapImage Avatar = new BitmapImage(new Uri($"{Constants.IconsPack}/avatar.png"));
+
         public static BitmapImage Bard = new BitmapImage(new Uri($"{Constants.IconsPack}/brd.png"));
+
         public static BitmapImage Blackmage = new BitmapImage(new Uri($"{Constants.IconsPack}/blm.png"));
+
         public static BitmapImage Blacksmith = new BitmapImage(new Uri($"{Constants.IconsPack}/bsm.png"));
+
         public static BitmapImage Botanist = new BitmapImage(new Uri($"{Constants.IconsPack}/btn.png"));
+
         public static BitmapImage Carpenter = new BitmapImage(new Uri($"{Constants.IconsPack}/cpt.png"));
+
         public static BitmapImage Chocobo = new BitmapImage(new Uri($"{Constants.IconsPack}/chocobo.png"));
+
         public static BitmapImage Conjurer = new BitmapImage(new Uri($"{Constants.IconsPack}/cnj.png"));
+
         public static BitmapImage Culinarian = new BitmapImage(new Uri($"{Constants.IconsPack}/cul.png"));
+
         public static BitmapImage DarkKnight = new BitmapImage(new Uri($"{Constants.IconsPack}/drk.png"));
+
         public static BitmapImage Dragoon = new BitmapImage(new Uri($"{Constants.IconsPack}/drg.png"));
+
         public static BitmapImage FateDefense = new BitmapImage(new Uri($"{Constants.IconsPack}/fate_defense.png"));
+
         public static BitmapImage FateEscort = new BitmapImage(new Uri($"{Constants.IconsPack}/fate_escort.png"));
+
         public static BitmapImage FateGather = new BitmapImage(new Uri($"{Constants.IconsPack}/fate_gather.png"));
+
         public static BitmapImage FateNM = new BitmapImage(new Uri($"{Constants.IconsPack}/fate_nm.png"));
+
         public static BitmapImage FateSlay = new BitmapImage(new Uri($"{Constants.IconsPack}/fate_slay.png"));
+
         public static BitmapImage Fisher = new BitmapImage(new Uri($"{Constants.IconsPack}/fsh.png"));
+
         public static BitmapImage Fishing = new BitmapImage(new Uri($"{Constants.IconsPack}/fishing.png"));
+
         public static BitmapImage Gathering = new BitmapImage(new Uri($"{Constants.IconsPack}/gathering.png"));
+
         public static BitmapImage Gladiator = new BitmapImage(new Uri($"{Constants.IconsPack}/gld.png"));
+
         public static BitmapImage Goldsmith = new BitmapImage(new Uri($"{Constants.IconsPack}/gsm.png"));
+
         public static BitmapImage Harvesting = new BitmapImage(new Uri($"{Constants.IconsPack}/harvesting.png"));
+
         public static BitmapImage HarvestingSuper = new BitmapImage(new Uri($"{Constants.IconsPack}/harvesting_super.png"));
+
         public static BitmapImage Lancer = new BitmapImage(new Uri($"{Constants.IconsPack}/lnc.png"));
+
         public static BitmapImage Leatherworker = new BitmapImage(new Uri($"{Constants.IconsPack}/ltw.png"));
+
         public static BitmapImage Logging = new BitmapImage(new Uri($"{Constants.IconsPack}/logging.png"));
+
         public static BitmapImage Machinist = new BitmapImage(new Uri($"{Constants.IconsPack}/mch.png"));
+
         public static BitmapImage Marauder = new BitmapImage(new Uri($"{Constants.IconsPack}/mrd.png"));
+
         public static BitmapImage Miner = new BitmapImage(new Uri($"{Constants.IconsPack}/min.png"));
+
         public static BitmapImage Mining = new BitmapImage(new Uri($"{Constants.IconsPack}/mining.png"));
+
         public static BitmapImage MiningSuper = new BitmapImage(new Uri($"{Constants.IconsPack}/mining_super.png"));
+
         public static BitmapImage MobAggressive1 = new BitmapImage(new Uri($"{Constants.IconsPack}/mob_aggressive_1.png"));
+
         public static BitmapImage MobAggressive2 = new BitmapImage(new Uri($"{Constants.IconsPack}/mob_aggressive_2.png"));
+
         public static BitmapImage MobAggressive3 = new BitmapImage(new Uri($"{Constants.IconsPack}/mob_aggressive_3.png"));
+
         public static BitmapImage MobAggressive4 = new BitmapImage(new Uri($"{Constants.IconsPack}/mob_aggressive_4.png"));
+
         public static BitmapImage MobAggressive5 = new BitmapImage(new Uri($"{Constants.IconsPack}/mob_aggressive_5.png"));
+
         public static BitmapImage MobAggressive6 = new BitmapImage(new Uri($"{Constants.IconsPack}/mob_aggressive_6.png"));
+
         public static BitmapImage MobClaimed = new BitmapImage(new Uri($"{Constants.IconsPack}/mob_claimed.png"));
+
         public static BitmapImage MobFate = new BitmapImage(new Uri($"{Constants.IconsPack}/mob_fate.png"));
+
         public static BitmapImage MobPassive1 = new BitmapImage(new Uri($"{Constants.IconsPack}/mob_passive_1.png"));
+
         public static BitmapImage MobPassive2 = new BitmapImage(new Uri($"{Constants.IconsPack}/mob_passive_2.png"));
+
         public static BitmapImage MobPassive3 = new BitmapImage(new Uri($"{Constants.IconsPack}/mob_passive_3.png"));
+
         public static BitmapImage MobPassive4 = new BitmapImage(new Uri($"{Constants.IconsPack}/mob_passive_4.png"));
+
         public static BitmapImage MobPassive5 = new BitmapImage(new Uri($"{Constants.IconsPack}/mob_passive_5.png"));
+
         public static BitmapImage MobPassive6 = new BitmapImage(new Uri($"{Constants.IconsPack}/mob_passive_6.png"));
+
         public static BitmapImage MobUnclaimed = new BitmapImage(new Uri($"{Constants.IconsPack}/mob_unclaimed.png"));
+
         public static BitmapImage Monk = new BitmapImage(new Uri($"{Constants.IconsPack}/mnk.png"));
+
         public static BitmapImage Ninja = new BitmapImage(new Uri($"{Constants.IconsPack}/nin.png"));
+
         public static BitmapImage Paladin = new BitmapImage(new Uri($"{Constants.IconsPack}/pld.png"));
+
         public static BitmapImage Player = new BitmapImage(new Uri($"{Constants.IconsPack}/player.png"));
+
         public static BitmapImage Pugilist = new BitmapImage(new Uri($"{Constants.IconsPack}/pug.png"));
+
         public static BitmapImage Quarrying = new BitmapImage(new Uri($"{Constants.IconsPack}/quarrying.png"));
+
         public static BitmapImage RadarHeading = new BitmapImage(new Uri($"{Constants.IconsPack}/radar_heading.png"));
+
         public static BitmapImage RedMage = new BitmapImage(new Uri($"{Constants.IconsPack}/rdm.png"));
+
         public static BitmapImage Rogue = new BitmapImage(new Uri($"{Constants.IconsPack}/rog.png"));
+
         public static BitmapImage Samurai = new BitmapImage(new Uri($"{Constants.IconsPack}/sam.png"));
+
         public static BitmapImage Scholar = new BitmapImage(new Uri($"{Constants.IconsPack}/sch.png"));
+
         public static BitmapImage SpearFishing = new BitmapImage(new Uri($"{Constants.IconsPack}/spearfishing.png"));
+
         public static BitmapImage SpearFishingSuper = new BitmapImage(new Uri($"{Constants.IconsPack}/spearfishing_super.png"));
+
         public static BitmapImage Summoner = new BitmapImage(new Uri($"{Constants.IconsPack}/smn.png"));
+
         public static BitmapImage Thaumaturge = new BitmapImage(new Uri($"{Constants.IconsPack}/thm.png"));
+
         public static BitmapImage Unknown = new BitmapImage(new Uri($"{Constants.IconsPack}/unknown.png"));
+
         public static BitmapImage Vendor = new BitmapImage(new Uri($"{Constants.IconsPack}/vendor.png"));
+
         public static BitmapImage Warrior = new BitmapImage(new Uri($"{Constants.IconsPack}/war.png"));
+
         public static BitmapImage Weaver = new BitmapImage(new Uri($"{Constants.IconsPack}/wvr.png"));
+
         public static BitmapImage Whitemage = new BitmapImage(new Uri($"{Constants.IconsPack}/whm.png"));
 
-        public static BitmapImage GetIconByName(string name)
-        {
-            try
-            {
+        public static BitmapImage GetIconByName(string name) {
+            try {
                 return new BitmapImage(new Uri($"{Constants.IconsPack}/{name.ToLowerInvariant()}.png"));
             }
-            catch (Exception)
-            {
+            catch (Exception) {
                 return Unknown;
             }
         }
 
-        public static string GetIconPackURI(string name)
-        {
+        public static string GetIconPackURI(string name) {
             return $"{Constants.IconsPack}/{name.ToLowerInvariant()}.png";
         }
     }
--- FFXIVAPP.ResourceFiles/Theme.cs
+++ FFXIVAPP.ResourceFiles/Theme.cs
@@ -1,59 +1,58 @@
-﻿// FFXIVAPP.ResourceFiles ~ Theme.cs
-// 
-// Copyright © 2007 - 2017 Ryan Wilson - All Rights Reserved
-// 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Theme.cs" company="SyndicatedLife">
+//   Copyright(c) 2018 Ryan Wilson &amp;lt;syndicated.life@gmail.com&amp;gt; (http://syndicated.life/)
+//   Licensed under the MIT license. See LICENSE.md in the solution root for full license information.
+// </copyright>
+// <summary>
+//   Theme.cs Implementation
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
-using System;
-using System.Windows.Media.Imaging;
+namespace FFXIVAPP.ResourceFiles {
+    using System;
+    using System.Windows.Media.Imaging;
 
-namespace FFXIVAPP.ResourceFiles
-{
-    public static class Theme
-    {
+    public static class Theme {
         public static BitmapImage ChineseFlag = new BitmapImage(new Uri($"{Constants.ThemePack}/cn.png"));
-        public static BitmapImage GermanFlag = new BitmapImage(new Uri($"{Constants.ThemePack}/de.png"));
-        public static BitmapImage EnglishFlag = new BitmapImage(new Uri($"{Constants.ThemePack}/en.png"));
-        public static BitmapImage FrenchFlag = new BitmapImage(new Uri($"{Constants.ThemePack}/fr.png"));
-        public static BitmapImage JapaneseFlag = new BitmapImage(new Uri($"{Constants.ThemePack}/ja.png"));
-        public static BitmapImage KoreanFlag = new BitmapImage(new Uri($"{Constants.ThemePack}/ko.png"));
-        public static BitmapImage RussianFlag = new BitmapImage(new Uri($"{Constants.ThemePack}/ru.png"));
 
         public static BitmapImage Default = new BitmapImage(new Uri($"{Constants.ThemePack}/default.png"));
+
         public static BitmapImage DefaultAvatar = new BitmapImage(new Uri($"{Constants.ThemePack}/default-avatar.jpg"));
+
         public static BitmapImage DefaultPluginLogo = new BitmapImage(new Uri($"{Constants.ThemePack}/default-plugin-logo.png"));
 
-        public static BitmapImage FFXIVAPPIcon = new BitmapImage(new Uri($"{Constants.ThemePack}/ffxivapp.ico"));
-        public static BitmapImage FFXIVAPPBase = new BitmapImage(new Uri($"{Constants.ThemePack}/ffxivapp-base.png"));
-        public static BitmapImage FFXIVAPPLogo = new BitmapImage(new Uri($"{Constants.ThemePack}/ffxivapp-logo.png"));
+        public static BitmapImage EnglishFlag = new BitmapImage(new Uri($"{Constants.ThemePack}/en.png"));
+
         public static BitmapImage FFXIVAPP = new BitmapImage(new Uri($"{Constants.ThemePack}/ffxivapp.png"));
+
+        public static BitmapImage FFXIVAPPBase = new BitmapImage(new Uri($"{Constants.ThemePack}/ffxivapp-base.png"));
+
+        public static BitmapImage FFXIVAPPIcon = new BitmapImage(new Uri($"{Constants.ThemePack}/ffxivapp.ico"));
+
+        public static BitmapImage FFXIVAPPLogo = new BitmapImage(new Uri($"{Constants.ThemePack}/ffxivapp-logo.png"));
+
+        public static BitmapImage FrenchFlag = new BitmapImage(new Uri($"{Constants.ThemePack}/fr.png"));
+
+        public static BitmapImage GermanFlag = new BitmapImage(new Uri($"{Constants.ThemePack}/de.png"));
+
+        public static BitmapImage JapaneseFlag = new BitmapImage(new Uri($"{Constants.ThemePack}/ja.png"));
+
+        public static BitmapImage KoreanFlag = new BitmapImage(new Uri($"{Constants.ThemePack}/ko.png"));
+
+        public static BitmapImage RussianFlag = new BitmapImage(new Uri($"{Constants.ThemePack}/ru.png"));
+
         public static BitmapImage Splash = new BitmapImage(new Uri($"{Constants.ThemePack}/splash.png"));
 
-        public static BitmapImage GetImageByName(string name)
-        {
-            try
-            {
+        public static BitmapImage GetImageByName(string name) {
+            try {
                 return new BitmapImage(new Uri($"{Constants.IconsPack}/{name.ToLowerInvariant()}.png"));
             }
-            catch (Exception)
-            {
+            catch (Exception) {
                 return Default;
             }
         }
 
-        public static string GetImagePackURI(string name)
-        {
+        public static string GetImagePackURI(string name) {
             return $"{Constants.ThemePack}/{name.ToLowerInvariant()}.png";
         }
     }
