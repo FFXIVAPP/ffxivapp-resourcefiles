48d189eedb726393cba5314ae7d2f44d046142eb
Ryan Wilson <syndicated.life@gmail.com>
2020-05-01 20:51:10 -0700

    fix: 🐛 copyright date and resharper changes

--- FFXIVAPP.ResourceFiles/Properties/AssemblyInfo.cs
+++ FFXIVAPP.ResourceFiles/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="AssemblyInfo.cs" company="SyndicatedLife">
-//   Copyright(c) 2018 Ryan Wilson &amp;lt;syndicated.life@gmail.com&amp;gt; (http://syndicated.life/)
+//   Copyright© 2007 - 2020 Ryan Wilson &amp;lt;syndicated.life@gmail.com&amp;gt; (https://syndicated.life/)
 //   Licensed under the MIT license. See LICENSE.md in the solution root for full license information.
 // </copyright>
 // <summary>
@@ -14,22 +14,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FFXIVAPP.ResourceFiles")]
-[assembly: AssemblyDescription("FFXIVAPP Image, Theme and File Resources")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SyndicatedLife")]
-[assembly: AssemblyProduct("FFXIVAPP.ResourceFiles")]
-[assembly: AssemblyCopyright("Copyright © 2007 - 2018 Ryan Wilson")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly : AssemblyTitle("FFXIVAPP.ResourceFiles"), ]
+[assembly : AssemblyDescription("FFXIVAPP Image, Theme and File Resources"), ]
+[assembly : AssemblyConfiguration(""), ]
+[assembly : AssemblyCompany("SyndicatedLife"), ]
+[assembly : AssemblyProduct("FFXIVAPP.ResourceFiles"), ]
+[assembly : AssemblyCopyright("Copyright © 2007 - 2020 Ryan Wilson"), ]
+[assembly : AssemblyTrademark(""), ]
+[assembly : AssemblyCulture(""), ]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly : ComVisible(false), ]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("38e2405f-58d4-411c-b1df-6e2d6f14082e")]
+[assembly : Guid("4F8437FC-EE41-4A15-80FC-93BF579A2E28"), ]
 
 // Version information for an assembly consists of the following four values:
 //      Major Version
@@ -39,5 +39,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.1")]
+[assembly : AssemblyVersion("2.0.0"), ]
+[assembly : AssemblyInformationalVersion("2.0.2"), ]
